b2dfd9ebfeecda36010fd242e78fb72843f64146
grupodetrabalhoeficiente <grupodetrabalhoeficiente@gmail.com>
2021-01-04 13:37:07 +0000

    danger

--- D213/Frontend/PedidoDeAjuda1.js
+++
@@ -1,9 +0,0 @@
-const x = document.getElementById("button")
-x.onclick = global checked;
-
-function checked {}
-if (check = 1)
-    xpto.style.display = block;
-check = 0;
-else
-    xpto.style.display = none;
