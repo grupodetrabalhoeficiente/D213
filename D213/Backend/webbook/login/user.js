60535b7c5c516d619a7ec38f3fa6f5837af1776c
grupodetrabalhoeficiente <grupodetrabalhoeficiente@gmail.com>
2021-01-19 20:13:26 +0000

    bye

--- D213/Backend/webbook/login/user.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');  
-var UserSchema = new mysql.Schema({  
-  name: String,
-  email: String,
-  password: String
-});
-mysql.model('User', UserSchema);
-module.exports = mysql.model('User');
--- D213/Backend/webbook/routes/login.route.js
+++
@@ -1,6 +0,0 @@
-const app = require('../app.js');
-const controllerUser = require('../controllers/usercontroller');
-const bodyParser = require('body-parser');
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
-const validator = require("../assets/Validations/validator");
-
