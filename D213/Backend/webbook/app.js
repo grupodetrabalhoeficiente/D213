e560956e90ba06b7ebbecdbdd7338dc731a1a7b7
grupodetrabalhoeficiente <grupodetrabalhoeficiente@gmail.com>
2021-01-24 13:43:24 +0000

    test

--- D213/Backend/webbook/app.js
+++ D213/Backend/webbook/app.js
@@ -19,8 +19,16 @@ app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 app.use(expressValidator());
 app.use(cookieParser());
-
-
+/*app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+})*/
+app.use(cors({
+    exposedHeaders: ['Location'],
+}));
 app.listen(port, function(err) {
     if (!err) {
         console.log('Your app is listening on ' + host + ' and port ' + port);
