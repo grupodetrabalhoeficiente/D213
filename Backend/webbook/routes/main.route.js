671e31fa54b29197186ea0e5f3b2714599b36650
grupodetrabalhoeficiente <grupodetrabalhoeficiente@gmail.com>
2021-01-24 14:58:30 +0000

    gotobeach

--- Backend/webbook/routes/main.route.js
+++ Backend/webbook/routes/main.route.js
@@ -1,14 +1,6 @@
 const {verify} = require('../controllers/middleware.js')
-const appfile = require('../app.js');
-const app = new appfile(app);
+const app = require('../app.js');
 const controllerOperational = require('../controllers/operational.controller.js');
-const bodyParser = require('body-parser');
-const express = require('express');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-
-
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
 app.route('/ranks')
     .get(verify,controllerOperational.readOperationalPoints)
