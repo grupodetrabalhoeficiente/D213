ee84b23dc9f41867aa72d7482b240a289d5a9d86
grupodetrabalhoeficiente <grupodetrabalhoeficiente@gmail.com>
2021-01-24 14:44:52 +0000

    go

--- Backend/webbook/routes/main.route.js
+++ Backend/webbook/routes/main.route.js
@@ -1,12 +1,11 @@
 const {verify} = require('../controllers/middleware.js')
-const app = require('../app.js');
+const appfile = require('../app.js');
+const app = new appfile(app);
 const controllerOperational = require('../controllers/operational.controller.js');
 const bodyParser = require('body-parser');
-const { check, validationResult } = require('express-validator');
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
-const validator = require("../assets/Validations/validator");
 
 
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
